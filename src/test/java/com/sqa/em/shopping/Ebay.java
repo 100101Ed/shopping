1f67bcab482862d04bee9925357590c1236f57cc
Dancalif <dancalif@gmail.com>
2016-03-01 17:40:25 -0800

    Update: test 1 and test 2 are done

--- src/test/java/com/sqa/em/shopping/Ebay.java
+++ src/test/java/com/sqa/em/shopping/Ebay.java
@@ -1,5 +1,7 @@
 package com.sqa.em.shopping;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -11,6 +13,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import junit.framework.Assert;
+
 public class Ebay {
 	// Declaring variables
 	public static String baseURL = "http://www.ebay.com/";
@@ -18,10 +22,11 @@ public class Ebay {
 	public static String userPassword = "testqa123";
 	private WebDriver driver;
 	public int attemptNum = 0;
-	public boolean foundBook = false;
-	public String zeroListings, itemNumber;
+	public boolean exitFlag = false;
+	public String zeroListings, itemNumber, itemNameOnSearch, itemNameInCart;
 	long numListing, itemNumberOnSearchPage, itemNumberOnWatchList;
 	public int pageNum = 1;
+	public int itemNum = 0;
 
 	@AfterClass
 	public void afterClass() {
@@ -69,6 +74,57 @@ public class Ebay {
 		Thread.sleep(1000);
 	}
 
+	@Test(enabled = false)
+	public void cartTest2() throws InterruptedException {
+		System.out.println("<<<<<<<<>>>>>>>>");
+		System.out.println("===Test 2: Shopping Cart functionality.===");
+		Actions actions = new Actions(driver);
+		Thread.sleep(3000);
+		WebElement mainMenu = driver.findElement(By.xpath("//*[@id='navigationFragment']/div/table/tbody/tr/td[9]"));
+		actions.moveToElement(mainMenu).build().perform();
+		WebElement subLink = driver.findElement(By.xpath("//a[contains(@title, 'Sporting Goods - Fishing')]"));
+		Thread.sleep(3000);
+		actions.click(subLink).build().perform();
+		driver.findElement(By.xpath("//*[@id='VisualNav']/a[2]/div")).click();
+
+		do {
+			Thread.sleep(3000);
+			List<WebElement> linksRodsItems = driver.findElements(By.className("vip"));
+			linksRodsItems.get(itemNum).click();
+			try {
+				Thread.sleep(3000);
+				if (driver.findElement(By.xpath("//*[@id='isCartBtn_btn']")).isDisplayed()) {
+					itemNameOnSearch = driver.findElement(By.id("itemTitle")).getText();
+					System.out.println("The name of item on Search page is " + itemNameOnSearch);
+					driver.findElement(By.xpath("//*[@id='isCartBtn_btn']")).click();
+					if (driver.findElement(By.xpath("//*[@id='pageLevelMessaing']/div/div/i")).isDisplayed()) {
+						itemNameInCart = driver
+								.findElement(By.xpath("//div[contains(@class, 'ff-ds3 fs16 mb5 fw-n sci-itmttl')]"))
+								.getText();
+						System.out.println("The name of item in a Cart is " + itemNameInCart);
+						if (itemNameOnSearch.equals(itemNameInCart)) {
+							System.out.println("Search item and item in a cart are the same.");
+							System.out.println("The test was succesfull.");
+							System.out.println("<<<<<<<<>>>>>>>>");
+							System.out.println("\n");
+							exitFlag = true;
+						} else {
+							Assert.fail("Search item and item in a cart don't match. Test failed.");
+							System.out.println("<<<<<<<<>>>>>>>>");
+							System.out.println("\n");
+							driver.quit();
+							System.exit(0);
+						}
+					}
+				}
+			} catch (Exception e) {
+				driver.navigate().back();
+				itemNum++;
+			}
+		} while (exitFlag == false);
+		driver.findElement(By.xpath("//*[@id='gh']/table/tbody/tr/td[1]")).click();
+	}
+
 	@DataProvider(name = "test1")
 	public Object[][] dp() {
 		// TODO: Implement method
@@ -78,11 +134,56 @@ public class Ebay {
 	@DataProvider(name = "test2")
 	public Object[][] dp2() {
 		// TODO: Implement method
-		return new Object[][] { { "Dan" } };
+		return new Object[][] { {} };
+	}
+
+	@DataProvider(name = "test3")
+	public Object[][] dp3() {
+		// TODO: Implement method
+		return new Object[][] { {} };
+	}
+
+	@Test(enabled = true)
+	public void myTest3() throws InterruptedException {
+		System.out.println("<<<<<<<<>>>>>>>>");
+		System.out.println("===Test 3: My test.===");
+		Actions actions = new Actions(driver);
+		Thread.sleep(3000);
+		WebElement mainMenu = driver.findElement(By.id("gh-ug"));
+		actions.moveToElement(mainMenu).build().perform();
+		WebElement subLink = driver.findElement(By.linkText("Account settings"));
+		Thread.sleep(3000);
+		actions.click(subLink).build().perform();
+		try {
+			if (!(driver
+					.findElement(By.xpath("//*[@id='mainContent']/table[4]/tbody/tr[2]/td/table/tbody/tr[2]/td[3]/a")))
+							.isDisplayed()) {
+				System.out.println("Looks like you don't have any billing info set.");
+				System.out.println(
+						"Then you actually don't need anything in watch list and cart. LEt me remove everything.");
+				driver.findElement(By.id("gh-cart-i")).click();
+				List<WebElement> linksItemsToRemove = driver.findElements(By.linkText("Remove"));
+				for (WebElement i : linksItemsToRemove) {
+					driver.findElement(By.xpath("/a[contains(@linkText, 'Remove')]")).click();
+				}
+
+			}
+		} catch (NoSuchElementException e) {
+			System.out.println("The billing info is set. then please go ahead and purchase something! :)");
+			System.out.println("Completing the test.");
+			System.out.println("<<<<<<<<>>>>>>>>");
+			// Quit Driver
+			// driver.close();
+			driver.quit();
+			System.exit(0);
+		}
+
 	}
 
 	@Test(enabled = false)
-	public void test1(String testName, String searchItem, String searchItemName) throws InterruptedException {
+	public void watchListTest1(String testName, String searchItem, String searchItemName) throws InterruptedException {
+		System.out.println("<<<<<<<<>>>>>>>>");
+		System.out.println("===Test 1: Watch list functionality.===");
 		// Verifying if our item exists in Ebay.
 		Thread.sleep(700);
 		// Search using keyword Java
@@ -133,11 +234,11 @@ public class Ebay {
 			Thread.sleep(700);
 
 			// Looking for a book with the keyword
-			while ((foundBook == false) && (pageNum != 4)) {
+			while ((exitFlag == false) && (pageNum != 4)) {
 				Thread.sleep(3000);
 				try {
 					if (driver.findElement(By.partialLinkText(searchItemName)).isDisplayed()) {
-						foundBook = true;
+						exitFlag = true;
 						// Click on the book if it's displayed
 						driver.findElement(By.partialLinkText(searchItemName)).click();
 						itemNumber = driver.findElement(By.id("descItemNumber")).getText().replaceAll("[^0-9]", "");
@@ -185,19 +286,5 @@ public class Ebay {
 			System.out.println("<<<<<<<<>>>>>>>>");
 			System.out.println("\n");
 		}
-	}
-
-	@Test(dataProvider = "test2")
-	public void test2(String name) throws InterruptedException {
-		Actions actions = new Actions(driver);
-		Thread.sleep(6000);
-		WebElement mainMenu = driver.findElement(By.xpath("//*[@id='navigationFragment']/div/table/tbody/tr/td[9]"));
-		actions.moveToElement(mainMenu);
-
-		WebElement subLink = driver.findElement(
-				By.xpath("//*[@id='navigationFragment']/div/table/tbody/tr/td[9]/div[2]/div[1]/ul[2]/li[2]/a"));
-		actions.moveToElement(subLink);
-		actions.click();
-		actions.perform();
 	}
 }
